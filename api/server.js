6572bdd387442c252d44d2ac63a89bbabada14a0
Robert Jank <robert.jank@yahoo.com>
2024-10-10 08:08:47 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,21 +1,21 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`
     <h2>Hubs API</h2>
     <p>Welcome to the Hubs API</p>
   `);
 });
 
-server.use('*', (req, res) => {
+server.use("*", (req, res) => {
   // catch all 404 errors middleware
   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 });
